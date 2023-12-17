dc9e07c11e382418db410d5b5798cd2f91ee8e24
Sergey Vishnyakov <sergey.vishnyakoff@gmail.com>
2023-12-16 20:37:19 -0500

    Allow sync script to be called from any directory

--- sync.sh
+++ sync.sh
@@ -1,6 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash 
 
 remote_repo=origin
+current_dir=$(pwd)
+script_dir=$(dirname "$0")
+
+# Temporary go to the directory with a script and which is managed by git
+cd "$script_dir"
 
 # Commits local changes
 function commit-local-changes() {
@@ -12,13 +17,13 @@ function commit-local-changes() {
 function merge-into-upstream() {
   echo "Pulling changes from the upstream"
 
-  if ! git pull --rebase ${remote_repo} main > /dev/null; then
+  if ! (cd "$script_dir" && git pull --rebase ${remote_repo} main > /dev/null); then
     echo "Conflicts with remote changes"
     return 1;
   fi
 
   echo "Pushing changes to upstream"
-  git push ${remote_repo} main > /dev/null
+  (cd "$script_dir" && git push ${remote_repo} main > /dev/null)
 }
 
 function main() {
@@ -27,3 +32,5 @@ function main() {
 }
 
 main
+# Go back to the directory that was set before current script was called
+cd "$current_dir"
